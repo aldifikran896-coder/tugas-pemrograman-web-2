dfad6386edf33a1c80ed1cc37f1fd321e9844b9f
aldifikran896-coder <aldifikran896@gmail.com>
2026-05-12 22:53:13 +0800

    feat: implementasi fungsi hapus data Phone

---
+++ resources/views/phones/edit.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h2>Edit Data HP</h2>
+
+<form action="{{ route('phones.update', $phone->id) }}" method="POST">
+
+    @csrf
+    @method('PUT')
+
+    <div class="mb-3">
+        <label>Brand</label>
+        <input type="text"
+               name="brand"
+               class="form-control"
+               value="{{ $phone->brand }}">
+    </div>
+
+    <div class="mb-3">
+        <label>Model</label>
+        <input type="text"
+               name="model"
+               class="form-control"
+               value="{{ $phone->model }}">
+    </div>
+
+    <div class="mb-3">
+        <label>Harga</label>
+        <input type="number"
+               name="price"
+               class="form-control"
+               value="{{ $phone->price }}">
+    </div>
+
+    <div class="mb-3">
+        <label>Stok</label>
+        <input type="number"
+               name="stock"
+               class="form-control"
+               value="{{ $phone->stock }}">
+    </div>
+
+    <div class="mb-3">
+        <label>RAM</label>
+        <input type="text"
+               name="ram"
+               class="form-control"
+               value="{{ $phone->ram }}">
+    </div>
+
+    <div class="mb-3">
+        <label>Storage</label>
+        <input type="text"
+               name="storage"
+               class="form-control"
+               value="{{ $phone->storage }}">
+    </div>
+
+    <div class="mb-3">
+        <label>Deskripsi</label>
+
+        <textarea name="description"
+                  class="form-control">{{ $phone->description }}</textarea>
+    </div>
+
+    <button class="btn btn-success">
+        Update
+    </button>
+
+</form>
+
+@endsection
